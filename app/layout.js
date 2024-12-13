0b642b02324197caf9553a74fd53802b82332584
Naleena <nknaleena101@gmail.com>
2024-12-13 09:09:02 +0530

    loginpage

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
   const header = (
     <header className="p-4 sm:p-8 flex items-center justify-between gap-4"> 
       <h1 className={'text-base sm:text-lg textGradient ' + Fugaz.className}>Mood Tracker</h1>
+      <div className="flex items-center justify-between">PLACEHOLDER</div>
     </header>
   )
 
