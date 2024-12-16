409a2faf771306db71ee9a54469f2918b61538a5
Naleena <nknaleena101@gmail.com>
2024-12-16 19:43:49 +0530

    firebase config

---
+++ context/AuthContext.js
@@ -0,0 +1,17 @@
+'use client'
+import React, {useContext, useState, useEffect} from 'react'
+
+const AuthContext = React.createContext()
+
+export function useAuth(){
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({children}) {
+    const value = 
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
