1976c7833b6bffc4528cfa1342005889dee6feda
Naleena <nknaleena101@gmail.com>
2024-12-16 20:33:45 +0530

    authContext done

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,4 +1,7 @@
 'use client'
+import { auth, db } from '@/firebase'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { doc, getDoc } from 'firebase/firestore'
 import React, {useContext, useState, useEffect} from 'react'
 
 const AuthContext = React.createContext()
@@ -8,7 +11,64 @@ export function useAuth(){
 }
 
 export function AuthProvider({children}) {
-    // const value = 
+    const [currentUser, setCurrentUser] = useState(null)
+    const [userDataObj, setUserDataObj] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    //Auth handlers
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout() {
+        setUserDataObj({});
+        setCurrentUser(null);
+        return signOut(auth)
+    }
+    
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async user => {
+            try {
+                //set the user to our local context state
+                setLoading(true)
+                setCurrentUser(user)
+                if(!user){
+                    return 
+                }
+
+                //if user exists, fetch data from firestore db
+                console.log('Fetching user data')
+                const docRef = doc(db, 'user', user.uid)
+                const docSnap = await getDoc(docRef)
+                let firebaseData = {}
+                if(docSnap.exists()){
+                    console.log('Found User Data')
+                    firebaseData = docSnap.data()
+                    console.log(firebaseData) //remove after testing
+                }
+                setUserDataObj(firebaseData)
+
+            } catch (err) {
+                console.log(err.message)
+            } finally{
+                setLoading(false)
+            }
+        })
+        return unsubscribe
+    })
+
+    const value = {
+        currentUser,
+        userDataObj,
+        signup,
+        logout,
+        login,
+        loading
+    } 
     return (
         <AuthContext.Provider value={value}>
             {children}
