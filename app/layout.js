0c312af31c5c4ef44e333b6ecac2f5b1f8863cc0
Naleena <nknaleena101@gmail.com>
2024-12-16 20:52:32 +0530

    firebase auth

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { AuthProvider } from "@/context/AuthContext";
 
 const Opensans = Open_Sans({
   variable: "--font-geist-sans",
@@ -10,7 +11,7 @@ const Opensans = Open_Sans({
 const Fugaz = Fugaz_One({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-  weight: ['400']
+  weight: ["400"],
 });
 
 export const metadata = {
@@ -21,29 +22,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   // creating a variable
   const header = (
-    <header className="p-4 sm:p-8 flex items-center justify-between gap-4"> 
-      <Link href={'/'}>
-        <h1 className={'text-base sm:text-lg textGradient ' + Fugaz.className}>Mood Tracker</h1>
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <Link href={"/"}>
+        <h1 className={"text-base sm:text-lg textGradient " + Fugaz.className}>
+          Mood Tracker
+        </h1>
       </Link>
       <div className="flex items-center justify-between">PLACEHOLDER</div>
     </header>
-  )
+  );
 
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
-      <p className={'text-indigo-400 ' + Fugaz.className}>Created with ❤</p>
+      <p className={"text-indigo-400 " + Fugaz.className}>Created with ❤</p>
     </footer>
-  )
-  
+  );
+
   return (
     <html lang="en">
-      <body
-        className={ 'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + Opensans.className}
-      >
-        {header}
-        {children}
-        {footer}
-      </body>
+      <AuthProvider>
+        <body
+          className={
+            "w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 " +
+            Opensans.className
+          }
+        >
+          {header}
+          {children}
+          {footer}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Fugaz_One } from 'next/font/google' 
 import Button from './Button';
 import Calendar from './Calendar';
+import Link from 'next/link';
 
 const Fugaz = Fugaz_One({
   variable: "--font-geist-mono",
@@ -11,14 +12,18 @@ const Fugaz = Fugaz_One({
 
 export default function Hero() {
   return (
-    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
+    <div className='py-4 md:py-10 flex flex-col gap-8 sm:gap-10'>
       <h1 className={'text-5xl sm:text-text-6xl md:text-7xl text-center ' + Fugaz.className}><span className='textGradient'>Mood Tracker</span> helps you track your <span className='textGradient'>daily</span> mood!</h1>
 
       <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>Create your mood record and see how you feel on <span className='font-semibold'>every day of every year</span></p>
 
       <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
-        <Button text="Sign Up"/>
-        <Button text="Login" dark/>
+        <Link href={'/dashboard'}>
+          <Button text="Sign Up"/>
+        </Link>
+        <Link href={'/dashboard'}>
+          <Button text="Login" dark/>
+        </Link>
       </div>
       <Calendar demo />
     </div>
